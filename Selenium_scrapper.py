dfcf4eb76d02c45a917cc26b89fad1f7bd91038c
Israel Adetubo <israeltubo@gmail.com>
2024-07-21 21:02:03 +0100

    Update Selenium_scrapper.py
    The Program functionality is now complete.

--- Selenium_scrapper.py
+++ Selenium_scrapper.py
@@ -1,5 +1,6 @@
 #this program will use selenium instead of beautiful soup to scrape the data from a site
 #This is to ensure that the scrapper program can interact with a dynamic site
+import sys
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
@@ -7,7 +8,9 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import time
 from selenium.webdriver.chrome.service import Service
-
+import pandas
+import pandas as pd
+#the path file of your driver
 PATH = "C:\\Program Files (x86)\\chromedriver.exe"
 
 # Create a Service object
@@ -22,8 +25,8 @@ search.send_keys(Keys.RETURN)
 pages=1
 not_last_pg=True
 page_titles=[]
-
-while not_last_pg==True:
+pages_URL=[]
+while not_last_pg:
     try:
         result = WebDriverWait(driver, 20).until(
             EC.presence_of_element_located((By.ID, "rso"))
@@ -35,8 +38,15 @@ while not_last_pg==True:
                 each_title=header.find_element(By.CSS_SELECTOR, "h3.LC20lb")
                 title_txt=each_title.text
                 page_titles.append(title_txt)
+                #we then get the tag for the link of the next page
+                try:
+                    each_link=header.find_element(By.TAG_NAME, "a")
+                    url=(each_link.get_attribute("href"))
+                    pages_URL.append(url)
+                except Exception as e:
+                    print(f"A link wasn't found for {title_txt}" )
             except Exception as e:
-                print("The Designated Element wasn't found" )
+                print(f"The Designated Element wasn't found in page {pages}" )
         #now we need to find the next page
         nxt_pg_button = WebDriverWait(driver, 20).until(
             EC.presence_of_element_located((By.ID, "pnnext"))
@@ -48,9 +58,19 @@ while not_last_pg==True:
     except Exception as e:
         print(f"The next Button is not present after {pages} pages")
         not_last_pg=False
-#quit driver and display all the titles
-print(page_titles)
+#quit driver and send all the titles and URL to an Excel file
 driver.quit()
-#we can also find elements by the text that will give a link upon clicking
-#this is done with the find_element(By.LINK_TEXT)
-#then link.click
+# Create a DataFrame to store scraped data
+df = pd.DataFrame({
+    'Page Titles': page_titles,
+    'URL': pages_URL
+})
+# Define the output file path for Excel
+output_file = r'C:\Users\TUBORR\Desktop\WEB3 DEV JOURNEY\PYTHON\Exercises\Web-Scraper\Google_titles_Url_Scraped.xlsx'
+# Write DataFrame to Excel
+try:
+    with pd.ExcelWriter(output_file, engine='xlsxwriter') as writer:
+        df.to_excel(writer, sheet_name='Search_results', index=False)
+    print(f"Data set has been scraped successfully into '{output_file}'. Thank you.")
+except Exception as e:
+    print(f"An error occurred when trying to store data: {e}")
